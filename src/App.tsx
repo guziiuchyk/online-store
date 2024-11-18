9f89f0205a708704483014c8df9b5f502264aab3
guziiuchyk <guziiuchyk@gmail.com>
2024-11-18 08:59:53 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import {RouterProvider} from "react-router-dom";
+import router from "./router.tsx";
+
+function App() {
+
+  return (
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+  )
+}
+
+export default App
